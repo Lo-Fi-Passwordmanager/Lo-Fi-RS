836edbdc5147a79de3fd4bf6e279e5ed2f0f1aa8
uxepd <uxepd@student.kit.edu>
2026-04-22 14:54:44 +0200

    refactor: remove unnecessary refcells and rcs

--- lofi/src/document/document.rs
+++ lofi/src/document/document.rs
@@ -1,7 +1,5 @@
 use std::cell::{RefCell};
 use std::collections::HashMap;
-use std::ops::{DerefMut};
-use std::rc::Rc;
 use tracing::debug;
 use crate::document::automerge_document::{
     AutomergeDoc, AutomergeEntry, AutomergeFolder, AutomergeItem,
@@ -23,51 +21,42 @@ pub trait ItemAttr {
 
 // TODO Split trait in public (items, getbyid) and non public (insert)
 pub trait FolderFunc {
-    fn insert(&mut self, item: Rc<RefCell<Item>>) -> ();
+    fn insert(&mut self, item: Item) -> ();
     fn items(&self) -> Vec<Item>;
     fn get_item_by_id(&self, id: &str) -> Option<Item>;
 }
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
-pub struct LofiDocument(Rc<RefCell<Folder>>);
+pub struct LofiDocument(Folder);
 
 impl FolderFunc for LofiDocument {
-    fn insert(&mut self, item: Rc<RefCell<Item>>) -> () {
-        self.0.borrow_mut().items.push(item);
+    fn insert(&mut self, item: Item) -> () {
+        self.0.items.push(item);
     }
 
     fn items(&self) -> Vec<Item> {
-        let items: Vec<Item> = self.0.borrow().items().iter()
-            .map(|rc| {
-                // 1. .borrow() gives a Ref<Item>
-                // 2. .clone() creates a new Item instance
-                rc.borrow().clone()
-            })
-            .collect();
-        items
+        self.0.items()
     }
 
     fn get_item_by_id(&self, id: &str) -> Option<Item> {
-        self.0.borrow().items.iter().find(|item| {
-            item.borrow().id() == id
-        }).cloned().map(|rc| {
-            rc.borrow().clone()
-        })
+        self.0.items.iter().find(|item| {
+            item.id() == id
+        }).cloned()
     }
 }
 
 impl LofiDocument {
     fn new() -> Self {
-        LofiDocument(Rc::from(RefCell::from(Folder{
+        LofiDocument(Folder{
             title: "".to_string(),
             id: "".to_string(),
             created_at: 0.0,
             edited_at: 0.0,
             items: Vec::new(),
-        })))
+        })
     }
 
-    fn folder(&self) -> Rc<RefCell<Folder>> {
+    fn folder(&self) -> Folder {
         self.0.clone()
     }
 }
@@ -97,10 +86,10 @@ impl FromAutomerge<AutomergeDoc, Self> for LofiDocument {
 
         for (item, len, path) in path_by_item {
             if len == 0 {
-                base.insert(Rc::from(RefCell::from(Item::from_automerge(item, crypter)?)))
+                base.insert(Item::from_automerge(item, crypter)?)
             } else {
                 let i = Item::from_automerge(item, crypter)?;
-                find_and_insert(&Rc::from(RefCell::from(Item::Folder(base.folder()))), &path, Rc::new(RefCell::new(i)));
+                find_and_insert(&Item::Folder(RefCell::from(base.folder())), &path, i);
             }
         }
 
@@ -109,25 +98,22 @@ impl FromAutomerge<AutomergeDoc, Self> for LofiDocument {
     }
 }
 
-fn find_and_insert(root: &Rc<RefCell<Item>>, path: &[String], new_node: Rc<RefCell<Item>>) -> bool {
-    let mut node = root.borrow_mut();
-
-    // If this is the node we are looking for, push the child
+fn find_and_insert(node: &Item, path: &[String], new_node: Item) -> bool {
     if path.len() == 1 && node.id() == path[0] {
-        match node.deref_mut() {
+        match node {
             Item::Entry(_) => {},
             Item::Folder(n) => {
-                n.clone().borrow_mut().items.push(new_node);
+                n.borrow_mut().items.push(new_node);
             }
         }
 
         return true;
     }
 
-    match node.deref_mut() {
+    match node {
         Item::Entry(_) => {},
         Item::Folder(n) => {
-            for child in &n.clone().borrow_mut().items {
+            for child in &n.borrow_mut().items {
                 if find_and_insert(child, &path[1..], new_node.clone()) {
                     return true;
                 }
@@ -139,8 +125,8 @@ fn find_and_insert(root: &Rc<RefCell<Item>>, path: &[String], new_node: Rc<RefCe
 
 #[derive(Debug, Clone, PartialOrd, PartialEq)]
 pub enum Item {
-    Folder(Rc<RefCell<Folder>>),
-    Entry(Rc<RefCell<Entry>>),
+    Folder(RefCell<Folder>),
+    Entry(RefCell<Entry>),
 }
 
 impl Item {
@@ -155,9 +141,9 @@ impl Item {
 impl FromAutomerge<AutomergeItem, Self> for Item {
     fn from_automerge(value: &AutomergeItem, crypter: &Crypter) -> LofiResult<Self> {
         match value {
-            AutomergeItem::WEntry(inner) => Ok(Item::Entry(Rc::from(RefCell::from(Entry::from_automerge(inner, crypter)?)))),
+            AutomergeItem::WEntry(inner) => Ok(Item::Entry(RefCell::from(Entry::from_automerge(inner, crypter)?))),
             AutomergeItem::WFolder(inner) => {
-                Ok(Item::Folder(Rc::from(RefCell::from(Folder::from_automerge(inner, crypter)?))))
+                Ok(Item::Folder(RefCell::from(Folder::from_automerge(inner, crypter)?)))
             }
         }
     }
@@ -169,7 +155,7 @@ pub struct Folder {
     id: String,
     created_at: f64,
     edited_at: f64,
-    items: Vec<Rc<RefCell<Item>>>,
+    items: Vec<Item>,
 }
 
 impl ItemAttr for Folder {
@@ -191,25 +177,18 @@ impl ItemAttr for Folder {
 }
 
 impl FolderFunc for Folder {
-    fn insert(&mut self, item: Rc<RefCell<Item>>) -> () {
+    fn insert(&mut self, item: Item) -> () {
         self.items.push(item);
     }
 
     fn items(&self) -> Vec<Item> {
-        let items: Vec<Item> = self.items.iter()
-            .map(|rc| {
-                rc.borrow().clone()
-            })
-            .collect();
-        items
+        self.items.clone()
     }
 
     fn get_item_by_id(&self, id: &str) -> Option<Item> {
         self.items.iter().find(|item| {
-            item.borrow().id() == id
-        }).cloned().map(|rc| {
-            rc.borrow().clone()
-        })
+            item.id() == id
+        }).cloned()
     }
 }
 
@@ -226,7 +205,7 @@ impl FromAutomerge<AutomergeFolder, Self> for Folder {
 }
 
 impl Folder {
-    fn items(&self) -> Vec<Rc<RefCell<Item>>> {
+    fn items(&self) -> Vec<Item> {
         self.items.clone()
     }
 }
